ddae4a35ac4198c358182c3e1d0251a79ef13dda
ozaki <fs65ugumori@yahoo.co.jp>
2019-06-25 07:25:30 +0900

    update sample

--- sample.js
+++ sample.js
@@ -1,12 +1,17 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+(async() => {
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.type('input[type="text"]', 'puppeteer');
-  await page.evaluate(() => {
-    document.querySelector('input[value="Google 検索"]').click();
+  await page.setRequestInterception(true);
+  page.on('request', request => {
+    if (request.resourceType() === 'image')
+      request.abort();
+    else
+      request.continue();
   });
+  await page.goto('https://news.google.com/news/');
+  await page.screenshot({path: 'news.png', fullPage: true});
+
   await browser.close();
 })();
